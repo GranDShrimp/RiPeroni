4715be9a65020d0be0c98ad5d1dbda3e56500bd8
Kaius Reed <kaikai11@live.com>
2015-02-07 17:59:21 -0500

    Revert "Got updated master"
    This reverts commit fd2e7bb97b772fda2c6d5b04829bb2db0e07fa01.

---
+++ RiPeroni Music Editor/RiPeroniStarter.java
@@ -0,0 +1,12 @@
+
+/** The base class for RiPeroni. Mostly used for testing Git.
+ * Possible delete later.
+ * @author Danny
+ *
+ */
+public class RiPeroniStarter {
+
+	public static void main (String[] args) {
+	}
+	
+}
---
+++ app/src/main/java/Song.java
@@ -0,0 +1,13 @@
+/**
+ * Created by J on 07-Feb-15.
+ */
+public class Song {
+    //Fields
+    private String title;
+    private String artist;
+    private String length;
+    //Needs actual song
+
+    //Constructor
+
+}
